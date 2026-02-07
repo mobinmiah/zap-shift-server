df082353a8c8b4f8c6d73bad3be788a31ab67cfa
mobinmiah <mobinmiah1990@gmail.com>
2026-02-08 02:14:03 +0600

    dashboard problem solved

--- index.js
+++ index.js
@@ -9,27 +9,22 @@ const port = process.env.PORT || 3000
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./zap-shift-firebase-adminsdk.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-
 function generateTrackingId() {
-    const prefix = "TE"; // your project code (TravelEase)
+    const prefix = "TE";
     const date = new Date().toISOString().slice(0, 10).replace(/-/g, "");
-
-    // Generate a secure 4-byte random hex (8 characters)
     const random = crypto.randomBytes(4).toString("hex").toUpperCase();
-
     return `${prefix}-${date}-${random}`;
 }
 
 module.exports = generateTrackingId;
 
-
-// middle wares
 app.use(cors())
 app.use(express.json())
 
@@ -51,10 +46,8 @@ const verifyFireBaseToken = async (req, res, next) => {
     }
 }
 
-// mongodb connection string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9hcy35q.mongodb.net/?appName=Cluster0`;
 
-// MongoClient with a MongoClientOptions 
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -65,18 +58,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-
         const db = client.db("zap_shift_db")
         const parcelCollection = db.collection("parcels")
         const paymentCollection = db.collection('payments')
         const userCollection = db.collection("users")
         const riderCollection = db.collection("riders")
         const trackingsCollection = db.collection("trackings")
+        const reviewsCollection = db.collection('reviews')
 
-        // verify admin middleware with database access
         const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded_email
+            const query = { email }
+            const user = await userCollection.findOne(query)
+
+            if (!user || user.role !== 'admin') {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
+            next()
+        }
+
+        const verifyRider = async (req, res, next) => {
             const email = req.decoded_email
             const query = { email }
             const user = await userCollection.findOne(query)
@@ -98,7 +99,6 @@ async function run() {
             return result
         }
 
-        // payment apis
         app.get('/payments', verifyFireBaseToken, async (req, res) => {
             const email = req.query.email;
             const query = {}
@@ -113,7 +113,6 @@ async function run() {
             res.send(result);
         })
 
-        // new
         app.post('/checkuot-sesion', async (req, res) => {
             const paymentInfo = req.body
             const amount = parseInt(paymentInfo.cost) * 100
@@ -143,86 +142,6 @@ async function run() {
             res.send({ url: session.url })
         })
 
-        // old
-        // app.post('/create-checkout-session', async (req, res) => {
-        //     const paymentInfo = req.body
-        //     const amount = parseInt(paymentInfo.cost)
-        //     const session = await stripe.checkout.sessions.create(
-        //         {
-        //             line_items: [
-        //                 {
-        //                     // Provide the exact Price ID (for example, price_1234) of the product you want to sell
-        //                     price_data: {
-        //                         currency: 'USD',
-        //                         unit_amount: amount,
-        //                         product_data: {
-        //                             name: paymentInfo.parcelName
-        //                         }
-        //                     },
-        //                     quantity: 1,
-        //                 },
-        //             ],
-        //             customer_email: paymentInfo.senderEmail,
-        //             mode: 'payment',
-        //             metadata: {
-        //                 parcelId: paymentInfo.parcelId
-        //             },
-        //             success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
-        //             cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
-        //         }
-        //     )
-        //     // console.log(session)
-        //     res.send({ url: session.url })
-        // })
-
-        // app.patch('/verify-payment-success', async (req, res) => {
-        //     const sessionId = req.query.session_id
-        //     const session = await stripe.checkout.sessions.retrieve(sessionId);
-        //     console.log("session retrieve", session)
-
-        //     const transactionId = session.payment_intent
-        //     const query = { transactionId: transactionId }
-        //     const paymentExist = await paymentCollection.findOne(query)
-        //     console.log(paymentExist)
-        //     if (paymentExist) {
-        //         return res.send({
-        //             message: 'Payment already exist, no need to add again'
-        //         })
-        //     }
-
-        //     const trackingId = generateTrackingId()
-        //     if (session.payment_status === 'paid') {
-        //         const id = session.metadata.parcelId
-        //         const query = { _id: new ObjectId(id) }
-        //         const update = {
-        //             $set: {
-        //                 paymentStatus: 'paid',
-        //                 trackingId: trackingId
-        //             }
-        //         }
-        //         const result = await parcelCollection.updateOne(query, update)
-
-        //         const payment = {
-        //             amount: session.amount_total / 100,
-        //             currency: session.currency,
-        //             customerEmail: session.customer_email,
-        //             parcelId: session.metadata.parcelId,
-        //             parcelName: session.metadata.parcelName,
-        //             transactionId: session.payment_intent,
-        //             paymentStatus: session.payment_status,
-        //             paidAt: new Date(),
-        //             trackingId: trackingId
-        //         }
-
-        //         if (session.payment_status === 'paid') {
-        //             const paymentResult = await paymentCollection.insertOne(payment)
-        //             res.send({ success: true, trackingId: trackingId, transactionId: session.payment_intent, modifyParcel: result, paymentInfo: paymentResult })
-        //         }
-
-        //         res.send(result)
-        //     }
-        //     res.send({ success: false })
-        // })
         app.patch('/verify-payment-success', async (req, res) => {
             try {
                 const sessionId = req.query.session_id;
@@ -231,7 +150,6 @@ async function run() {
                 const transactionId = session.payment_intent;
                 const existingPayment = await paymentCollection.findOne({ transactionId });
 
-                // Already paid → avoid duplicate insert
                 if (existingPayment) {
                     return res.send({
                         success: true,
@@ -240,12 +158,10 @@ async function run() {
                     });
                 }
 
-                // Only continue if paid
                 if (session.payment_status !== 'paid') {
                     return res.send({ success: false, message: "Payment not completed" });
                 }
 
-                // Update parcel with tracking & paid status
                 const trackingId = session.metadata.trackingId;
                 const parcelId = session.metadata.parcelId;
 
@@ -255,15 +171,13 @@ async function run() {
                         $set: {
                             paymentStatus: "paid",
                             deliveryStatus: 'pending_pickup',
-
                             transactionId: transactionId
                         }
                     }
                 );
 
-                logTracking(trackingId, 'pending_pickup')
+                logTracking(trackingId, 'parcel_paid')
 
-                // Create payment record
                 const payment = {
                     amount: session.amount_total / 100,
                     currency: session.currency,
@@ -286,14 +200,12 @@ async function run() {
                 });
 
             } catch (err) {
-                console.error(err);
                 res.status(500).send({ success: false, error: err.message });
             }
-        });
+        })
 
 
-        // parcel apis
-        app.get('/parcels', async (req, res) => {
+        app.get('/parcels', verifyFireBaseToken, async (req, res) => {
             const query = {}
             const { email, deliveryStatus } = req.query
 
@@ -319,7 +231,6 @@ async function run() {
                 query.riderEmail = riderEmail
             }
             if (deliveryStatus !== "parcel_delivered") {
-                // query.deliveryStatus = { $in: ['driver_assigned', 'rider_on_the_way'] }
                 query.deliveryStatus = { $nin: ["parcel_delivered"] }
             }
             else {
@@ -335,6 +246,25 @@ async function run() {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await parcelCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.get('/parcels/delivery-status/stats', verifyFireBaseToken, verifyAdmin, async (req, res) => {
+            const pipeline = [
+                {
+                    $group: {
+                        _id: '$deliveryStatus',
+                        count: { $sum: 1 }
+                    }
+                },
+                {
+                    $project: {
+                        status: '$_id',
+                        count: 1,
+                    }
+                }
+            ]
+            const result = await parcelCollection.aggregate(pipeline).toArray()
             res.send(result)
         })
 
@@ -363,7 +293,6 @@ async function run() {
             }
             const result = await parcelCollection.updateOne(query, updatedDoc)
 
-            // update rider info
             const riderQuery = { _id: new ObjectId(riderId) }
             const riderUpdatedDoc = {
                 $set: {
@@ -408,18 +337,15 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
 
-            // Free the rider
             const parcel = await parcelCollection.findOne(query);
 
             if (parcel?.riderId) {
-
                 await riderCollection.updateOne(
                     { _id: new ObjectId(parcel.riderId) },
                     { $set: { workStatus: "available" } }
                 );
             }
 
-            // Update parcel
             const updatedDoc = {
                 $set: {
                     deliveryStatus: deliveryStatus,
@@ -436,9 +362,6 @@ async function run() {
         });
 
 
-
-
-
         app.delete('/parcels/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
@@ -446,13 +369,11 @@ async function run() {
             res.send(result)
         })
 
-        // user apis
         app.get('/users', verifyFireBaseToken, async (req, res) => {
             const searchText = req.query.searchText
             const query = {}
 
             if (searchText) {
-                // query.displayName = { $regex: searchText, $options: 'i' }
                 query.$or = [
                     { displayName: { $regex: searchText, $options: 'i' } },
                     { email: { $regex: searchText, $options: 'i' } },
@@ -463,10 +384,6 @@ async function run() {
             const result = await cursor.toArray()
             res.send(result)
         })
-
-        // app.get('/users/:id', async (req, res) => {
-
-        // })
 
         app.get('/users/:email/role', verifyFireBaseToken, async (req, res) => {
             const email = req.params.email
@@ -501,7 +418,6 @@ async function run() {
             res.send(result)
         })
 
-        // rider apis
         app.get('/riders', async (req, res) => {
             const { status, district, workStatus } = req.query
             const qurey = {}
@@ -520,6 +436,54 @@ async function run() {
 
             const cursor = riderCollection.find(qurey)
             const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/riders/delivery-per-day', async (req, res) => {
+            const email = req.query.email
+
+            const pipeline = [
+                {
+                    $match: {
+                        riderEmail: email,
+                        deliveryStatus: "parcel_delivered"
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "trackings",
+                        localField: "trackingId",
+                        foreignField: "trackingId",
+                        as: "parcel_trackings"
+                    }
+                },
+                {
+                    $unwind: "$parcel_trackings"
+                },
+                {
+                    $match: {
+                        "parcel_trackings.status": "parcel_delivered"
+                    }
+                },
+                {
+                    $addFields: {
+                        deliveryDay: {
+                            $dateToString: {
+                                format: "%Y-%m-%d",
+                                date: "$parcel_trackings.createdAt"
+                            }
+                        }
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$deliveryDay",
+                        deliveredCount: { $sum: 1 }
+                    }
+                }
+            ];
+
+            const result = await parcelCollection.aggregate(pipeline).toArray()
             res.send(result)
         })
 
@@ -574,7 +538,6 @@ async function run() {
             res.send(result)
         })
 
-        // tracking apis
         app.get('/trackings/:trackingId/logs', async (req, res) => {
             const trackingId = req.params.trackingId
             const query = { trackingId }
@@ -582,21 +545,25 @@ async function run() {
             res.send(result)
         })
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
     }
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-    res.send('Zap Shift is runnign!')
+    res.send('Zap Shift is running!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
+} else {
+    module.exports = app;
+}
